9677482f10c87fd3d3a380a9863a4d9048af8607
Kamfeskaya <tatendachris@gmail.com>
2025-02-10 19:18:02 +0200

    feat: add dataset export

--- src/app/gigs/[id]/components/Header.tsx
+++ src/app/gigs/[id]/components/Header.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { useFormContext } from '@/forms-builder/context';
-import { Flex } from '@radix-ui/themes';
-import { Save } from 'lucide-react';
-import { FormBuilderHeaderProps } from './types';
-import { Button } from '@/components/ui/aria-components/Button';
-import { Badge } from '@/components/ui/badge';
-import { Separator } from '@/components/ui/aria-components/Separator';
-import { useRouter } from 'next/navigation';
-import { Caption } from '@/components/design-sytem/typography';
+import {useFormContext} from '@/forms-builder/context';
+import {Button, DropdownMenu, Flex} from '@radix-ui/themes';
+import {Save} from 'lucide-react';
+import {FormBuilderHeaderProps} from './types';
+import {Badge} from '@/components/ui/badge';
+import {Separator} from '@/components/ui/aria-components/Separator';
+import {useRouter} from 'next/navigation';
+import {Caption} from '@/components/design-sytem/typography';
 import Symbol from '@/components/icons/symbol';
-import { useEffect, useState } from 'react';
-import { toast } from 'sonner';
+import {useEffect, useState} from 'react';
+import {toast} from 'sonner';
+import axiosInstance from '@/hooks/useApiFetcher';
 
 const GigHeader = ({
 	save,
@@ -23,10 +23,11 @@ const GigHeader = ({
 	responsesCount,
 	pendingResponsesCount,
 }: FormBuilderHeaderProps) => {
-	const { form, exportForm } = useFormContext();
+	const {form, exportForm} = useFormContext();
 	const navigation = useRouter();
 	const [showSavingMessage, setShowSavingMessage] = useState(false);
 	const [showPublishedMessage, setShowPublishedMessage] = useState(false);
+	const [isExporting, setIsExporting] = useState(false);
 
 	const onSaveChanges = async () => {
 		save(exportForm());
@@ -36,38 +37,84 @@ const GigHeader = ({
 		publish(form);
 	};
 
+	const onDownload = async (format: 'csv' | 'xlsx') => {
+		setIsExporting(true);
+		const url = `/admin/gigs/${gig._id}/responses/export-all?format=${format}`;
+		const response = await axiosInstance.getAndReturnHeaders(
+			url,
+			// @ts-ignore
+			{
+				responseType: 'blob',
+			},
+		);
+
+		const blob = new Blob([response.data], {
+			type: response.headers['content-type'],
+		});
+
+		const serverFileName =
+			response.headers['content-disposition']?.split('filename=')[1];
+
+		const fileName =
+			serverFileName?.replace(/["]/g, '') || `gig-responses.${format}`;
+
+		const link = document.createElement('a');
+		link.href = window.URL.createObjectURL(blob);
+		link.download = fileName;
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+
+		return setIsExporting(false);
+	};
+
+	const onOpenInSheets = async () => {
+		setIsExporting(true);
+		const url = `/admin/gigs/${gig._id}/responses/export-all?exportTo=google-sheets`;
+		const response = await axiosInstance.get(url);
+
+		window.open(response.sheetUrl, '_blank');
+		setIsExporting(false);
+	};
 
 	return (
 		<div className="py-1.5 px-4 justify-between">
-			<div className="flex justify-between mb-2">
+			<div className="flex justify-between mb-2 items-center">
 				<div className="flex">
 					<Button
-						onPress={navigation.back}
+						onClick={navigation.back}
+						color="gray"
 						variant="ghost">
 						Back to Gigs
 					</Button>
 				</div>
 				<div
 					className="flex items-center justify-between"
-					style={{ gap: '8px' }}>
+					style={{gap: '8px'}}>
 					<div className="flex gap-4">
+						<ExportDropdown
+							onDownload={onDownload}
+							onOpenInSheets={onOpenInSheets}
+							isExporting={isExporting}
+						/>
 						<Button
-							variant="secondary"
-							isPending={isSaving}
-							onPress={onSaveChanges}>
-							<Symbol className='!text-base'>save</Symbol>
+							variant="outline"
+							color="gray"
+							loading={isSaving}
+							onClick={onSaveChanges}>
+							<Symbol className="!text-base">download</Symbol>
 							Save Form
 						</Button>
 						<Button
-							variant={
+							color={
 								status === 'DRAFT'
-									? 'secondary'
+									? 'gray'
 									: status === 'ACTIVE'
-										? 'destructive'
-										: 'secondary'
+										? 'red'
+										: 'gray'
 							}
-							isPending={isPublishing}
-							onPress={onPublish}>
+							loading={isPublishing}
+							onClick={onPublish}>
 							{status === 'ACTIVE' && <Symbol>pause</Symbol>}
 							{status === 'DRAFT'
 								? 'Enable submissions'
@@ -85,7 +132,7 @@ const GigHeader = ({
 						{gig.name}
 					</p>
 					<Badge
-						style={{ fontWeight: '500' }}
+						style={{fontWeight: '500'}}
 						color={
 							status === 'DRAFT'
 								? 'blue'
@@ -115,7 +162,51 @@ const GigHeader = ({
 	);
 };
 
-const Stat = ({ name, value }: { name: string; value: string | number }) => (
+const ExportDropdown = ({
+	onDownload,
+	onOpenInSheets,
+	isExporting = false,
+}: {
+	onDownload: (format: 'csv' | 'xlsx') => void;
+	onOpenInSheets: () => void;
+	isExporting: boolean;
+}) => {
+	return (
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger>
+				<Button
+					variant="outline"
+					color="gray">
+					Export Dataset
+					<DropdownMenu.TriggerIcon />
+				</Button>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content>
+				<DropdownMenu.Item
+					disabled={isExporting}
+					onClick={() => onDownload('csv')}>
+					<Symbol>csv</Symbol>
+					Download CSV
+				</DropdownMenu.Item>
+				<DropdownMenu.Item
+					disabled={isExporting}
+					onClick={() => onDownload('xlsx')}>
+					<Symbol>table</Symbol>
+					Download XSLX
+				</DropdownMenu.Item>
+				<DropdownMenu.Separator />
+				<DropdownMenu.Item
+					disabled={isExporting}
+					onClick={onOpenInSheets}>
+					<Symbol className="!text-base">arrow_outward</Symbol>
+					Open in Google Sheets
+				</DropdownMenu.Item>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+	);
+};
+
+const Stat = ({name, value}: {name: string; value: string | number}) => (
 	<div className="border-2 border-surface-secondary py-2 px-4 rounded-xl">
 		<Caption className="text-content-tertiary text-xs uppercase !font-medium">
 			{name}
